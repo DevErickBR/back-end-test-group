7433eb85bac754db1cd3235f82db1f1f77b3757e
Leonardo Reis <amarantee.reis@gmail.com>
2025-03-24 09:37:49 -0300

    feat: register use case

--- src/components/user/use-cases/register/register.use-case.ts
+++ src/components/user/use-cases/register/register.use-case.ts
@@ -1,13 +1,22 @@
-import { NextFunction, Request } from "express";
 import { UserRepository } from "../../repositories/user.repository";
+import * as bcrypt from "bcryptjs";
+import { BadRequest } from "../../../../errors/BadRequest";
+
 export class RegisterUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async handle(request: Request, response: Response, next: NextFunction) {
-    try {
-      const { email, password } = request.body;
-    } catch (error) {
-      next(error);
-    }
+  async handle({ email, name, password }: any) {
+    const user = await this.userRepository.findByEmail(email);
+    if (user) throw new BadRequest("User already exists");
+
+    const hashedPassword = bcrypt.hash(password, 8);
+
+    await this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return { message: "User created successfully" };
   }
 }
