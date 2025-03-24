b2e7361d3d3cc4ed9b344cc3ec0a6ce2aeaaa8d0
Leonardo Reis <amarantee.reis@gmail.com>
2025-03-24 09:40:10 -0300

    feat: login use case

--- src/components/user/use-cases/login/login.use-case.ts
+++ src/components/user/use-cases/login/login.use-case.ts
@@ -1,13 +1,25 @@
-import { NextFunction, Request } from "express";
+import { BadRequest } from "../../../../errors/BadRequest";
+import { UserRepository } from "../../repositories/user.repository";
+import * as bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { env } from "../../../../env";
 
 export class LoginUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async handle(request: Request, response: Response, next: NextFunction) {
-    try {
-      const { email, password } = request.body;
-    } catch (error) {
-      next(error);
-    }
+  async handle(data: any) {
+    const { email, password } = data;
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) throw new BadRequest("Invalid credentials");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) throw new BadRequest("Invalid credentials");
+
+    const token = jwt.sign({ id: user.id }, env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return { token };
   }
 }
